18a99c99e36fc183ec95cd6cf0fed13db2222fcd
louazzanimaryem <maryemlouazzani@gmail.com>
2025-12-26 23:38:59 +0100

    done

---
+++ basicjs/copiedCharacter.js
@@ -0,0 +1,10 @@
+
+let character = "Hello";
+console.log(character);
+
+let secondCharacter = character;  
+console.log(secondCharacter);
+
+character = "World";
+console.log(character);
+console.log(secondCharacter);
